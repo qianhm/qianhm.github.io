afbdc2ad4a9f371f752e2c181de16cf95d889e8e
qianhm <qianhm@users.noreply.github.com>
2026-05-21 15:47:24 +0800

    Add custom components: navbar, footer with vercount, home cards

---
+++ components/HomeCards.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const avatarUrl = '/blog/avatar.jpg'
+</script>
+
+<template>
+  <img :src="avatarUrl" alt="avatar" class="w-16 h-16 rounded-full">
+</template>
---
+++ components/YunNimboNavMenu.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useAppStore } from 'valaxy'
+import { useSiteConfig } from 'valaxy'
+
+const app = useAppStore()
+const siteConfig = useSiteConfig()
+</script>
+
+<template>
+  <nav class="yun-nav-menu flex items-center gap-1">
+    <RouterLink to="/" class="yun-icon-btn" title="首页">
+      <div i-ri-home-4-line />
+    </RouterLink>
+
+    <button
+      class="yun-icon-btn"
+      :title="app.isDark ? '切换亮色模式' : '切换暗色模式'"
+      @click="app.toggleDark()"
+    >
+      <div i="ri-sun-line dark:ri-moon-line" />
+    </button>
+
+    <YunSearchTrigger v-if="siteConfig.search.enable" />
+    <YunToggleLocale />
+  </nav>
+</template>
